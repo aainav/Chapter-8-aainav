5379ad4c1c9c51b3f949ef89a6eacc48ea390476
aainav <31443617+aainav@users.noreply.github.com>
2018-04-19 08:24:19 -0500

    do fractal

--- Chapter 8/Ch 8 Project/Fundraiser.java
+++ Chapter 8/Ch 8 Project/Fundraiser.java
@@ -11,14 +11,19 @@ public class Fundraiser
     public static int collect(int monataryGoal)
     {
         System.out.println(monataryGoal);
-        
+        int total = 0;
         if(monataryGoal == 1)
         {
             return 1;
         }
         else
         {
-            return collect(monataryGoal/10) + collect(monataryGoal/10) + collect(monataryGoal/10); //do this for 10; 
+            for(int i = 0; i < 10; i++)
+            {
+                total += collect(monataryGoal/10); //do this for 10; 
+            }
+            return total + 1;
         }
+        
     }
 }
